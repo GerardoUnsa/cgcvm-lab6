b65c816f77ea9e1bc6f44c233402250fa9f7af82
Gerardo <gportocarrerob@unsa.edu.pe>
2022-06-08 15:52:28 -0500

    first commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float moveH, moveV;
+    [SerializeField] private float moveSpeed = 2.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveH = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        moveV = Input.GetAxisRaw("Vertical") * moveSpeed;
+    }
+
+    private void FixedUpdate() {
+        rb.velocity = new Vector2(moveH, moveV);
+    }
+}
